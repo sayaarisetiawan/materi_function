437a88e9a3ca0a90a6507febe17702b99bfca33a
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-13 22:51:27 +0700

    task 3

--- date.php
+++ date.php
@@ -1,22 +1,22 @@
 <?php
-// echo date ("l");
-// echo time();
-
-// echo date (
-//     "l, d-M-Y",
-//     time() + 
-//     60 * 60 * 2400
-// ); seratus hari setelah hari ini
-
-// echo date (
-//     "l, d-M-Y",
-//     time() + 
-//     60 * 60 * 24000
-// ); seribu hari setelah hari ini
+echo date ("l");
+echo time();
 
 echo date (
     "l, d-M-Y",
     time() + 
-    60 * 60 * -12000
-);
+    60 * 60 * 2400); 
+    // seratus hari setelah hari ini
+
+echo date (
+    "l, d-M-Y",
+    time() + 
+    60 * 60 * 24000);
+    // seribu hari setelah hari ini
+
+echo date (
+    "l, d-M-Y",
+    time() + 
+    60 * 60 * -12000); 
+    // limaratus hari sebelum hari ini
 ?>
