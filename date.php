bc234238bec241e873ae68ac3a65ba9f0cd24e1a
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-13 22:42:19 +0700

    task 3

--- date.php
+++ date.php
@@ -2,9 +2,15 @@
 // echo date ("l");
 // echo time();
 
+// echo date (
+//     "l, d-M-Y",
+//     time() + 
+//     60 * 60 * 2400
+// );
+
 echo date (
     "l, d-M-Y",
     time() + 
-    60 * 60 * 2400
+    60 * 60 * 24000
 );
 ?>
