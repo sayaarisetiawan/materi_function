0d44d123768912924bf50fcf1cd0d24ab770dfa0
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-14 13:27:24 +0700

    task 8

---
+++ fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkalian($angka1 , $angka2)
+{
+    return $hasil = $angka1 * $angka2;
+}
+$hasil = perkalian(4, 5);
+echo "perkalian 4 x 5 adalah $hasil";
+echo"<br>";
+echo "perkalian 9 x2 adalah ", perkalian (9, 2);
