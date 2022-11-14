9d05f0a67380ddf07a271c92982522d3b479efd1
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-14 13:35:12 +0700

    edit

--- date.php
+++ date.php
@@ -1,26 +1,27 @@
 <?php
-// echo date ("l");
-// echo time();
+echo date ("l");
+echo"<br>";
+echo time();
+echo"<br>";
 
-// echo date (
-//     "l, d-M-Y",~
-//     time() + 
-//     60 * 60 * 2400); 
-//     // seratus hari setelah hari ini
+echo date (
+    "l, d-M-Y",
+    time() + 60 * 60 * 24 * 100); 
+    // seratus hari setelah hari ini
+    echo"<br>";
+echo date (
+    "l, d-M-Y",
+    time() + 60 * 60 * 24 * 1000);
+    // seribu hari setelah hari ini
+    echo"<br>";
+echo date (
+    "l, d-M-Y",
+    time() + 60 * 60 * 24 * -500); 
+    // limaratus hari sebelum hari ini
+    echo"<br>";
 
-// echo date (
-//     "l, d-M-Y",
-//     time() + 
-//     60 * 60 * 24000);
-//     // seribu hari setelah hari ini
-
-// echo date (
-//     "l, d-M-Y",
-//     time() + 
-//     60 * 60 * -12000); 
-//     // limaratus hari sebelum hari ini
-
-// echo date ("l, d-M-Y " , mktime(0,0,0, 03 , 31-10, 2000));
+echo date ("l, d-M-Y " , mktime(0,0,0, 03 , 31-10, 2000));
+echo"<br>";
 
 $date = strtotime('21-03-2000'); 
 $newDate = date('l,d-m-Y', strtotime("+30 year", $date)); 
