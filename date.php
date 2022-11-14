5417c99b642bad935e7acd6692b9721157058346
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-14 11:42:06 +0700

    task 6

--- date.php
+++ date.php
@@ -20,5 +20,9 @@
 //     60 * 60 * -12000); 
 //     // limaratus hari sebelum hari ini
 
-echo date ("l, d-M-Y " , mktime(0,0,0, 03 , 31-10, 2000));
+// echo date ("l, d-M-Y " , mktime(0,0,0, 03 , 31-10, 2000));
+
+$date = strtotime('21-03-2000'); 
+$newDate = date('l,d-m-Y', strtotime("+30 year", $date)); 
+echo 'Ultah saya di 2030 : ' , $newDate;
 ?>
